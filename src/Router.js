6ee22eaff145e593060cfefae331e684fb0a709a
Meagan Pau <meaganpau@Meagans-MapBook-Pro.local>
2026-09-21 22:14:19 -0400

    lint

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import PokemonStats from './pages/PokemonStats';
@@ -9,8 +9,6 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
-                {/* <Redirect exact from="/product" to="/product/incorporation" />
-                <Route path="/:group?" component={LandingRouter} /> */}
                 <Route exact path='/pokemon/:name' component={PokemonStats} />
                 <Route exact path='/type/:type' component={Type} />
                 <Route exact path='/' component={Home} />
